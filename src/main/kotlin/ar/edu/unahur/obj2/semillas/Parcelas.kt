2cf9ff4409d91894c517b85767bc4386e02dacae
Carolina Luján <carolinaanahi.lujan@estudiantes.unahur.edu.ar>
2022-04-10 16:43:41 -0300

    Clase parcela arreglada

--- src/main/kotlin/ar/edu/unahur/obj2/semillas/Parcelas.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/semillas/Parcelas.kt
@@ -3,7 +3,7 @@ package ar.edu.unahur.obj2.semillas
 class Parcelas(val ancho: Double, val largo: Double, val horasDeSol: Int) {
     var plantas = mutableListOf<Planta>()
 
-    fun superficie() { largo * ancho }
+    fun superficie(): Double { return largo * ancho }
     fun cantidadMaximaDePlantas() {
         if(ancho > largo) {
             this.superficie() / 5
@@ -11,15 +11,13 @@ class Parcelas(val ancho: Double, val largo: Double, val horasDeSol: Int) {
             (this.superficie() / 3) * largo
         }
     }
-    fun tieneComplicaciones() { plantas.any() { it.horasDeSolQueTolera() < horasDeSol } //?
+    fun tieneComplicaciones() { plantas.any() { it.horasDeSolQueTolera() < horasDeSol } }//?
     fun cantidadDePlantas() { plantas.size }
     fun plantarUnaPlanta(planta: Planta) {
         if (this.cantidadDePlantas() == this.cantidadMaximaDePlantas()) {
             println("No hay lugar en esta parcela")
-        } else if (horasDeSol > planta.horasDeSolQueTolera() + 2) {
-            println("No se puede plantar acá")
         } else {
             plantas.add(planta) }
         }
     }
-}
+
