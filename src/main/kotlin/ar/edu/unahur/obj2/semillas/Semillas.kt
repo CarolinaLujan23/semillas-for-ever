56e20a7aa7ba447df90fbecc7e8a6e664f96ebd2
Carolina Luján <carolinaanahi.lujan@estudiantes.unahur.edu.ar>
2022-04-10 15:48:34 -0300

    Falta la quinoa todavía

--- src/main/kotlin/ar/edu/unahur/obj2/semillas/Semillas.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/semillas/Semillas.kt
@@ -1,37 +1,40 @@
 package ar.edu.unahur.obj2.semillas
 
-class Menta(var altura: Double, val anioSemilla: Int) {
-    fun espacio(): Double {
-        TODO("Te la debo, amigue...")
-    }
-
-    fun daSemillas(): Boolean {
-        TODO("Colgue, che...")
-    }
-
-    fun esFuerte(): Boolean {
-        TODO("Estas seguro que lo queres implementar así?")
-    }
-
+abstract class Menta(altura: Double, anioSemilla: Int): Planta(altura, anioSemilla) {
+    override fun espacio(): Double { return altura + 1 }
+    override fun daSemillas(): Boolean { return altura > 0.4 || super.daSemillas() }
 }
 
-class Soja(var altura: Double, val anioSemilla: Int) {
-    fun espacio(): Double {
+abstract class Soja(altura: Double, anioSemilla: Int): Planta(altura, anioSemilla) {
+    override fun espacio(): Double { return altura / 2 }
+    override fun daSemillas(): Boolean { return super.daSemillas() && anioSemilla > 2007 && altura in 0.75..0.9 }
+    override fun horasDeSolQueTolera(): Int {
+        val horasBase = when {
+            altura < 0.5 -> 6
+            altura < 1 -> 8
+            else -> 12
+        }
+        return horasBase
+    }
+}
+
+abstract class Quinoa(altura: Double, anioSemilla: Int): Planta(altura, anioSemilla) {
+    override fun espacio(): Double {
         TODO("Lee un poco más el enunciado...")
     }
 
-    fun daSemillas(): Boolean {
+    override fun daSemillas(): Boolean {
         TODO("Dalee")
     }
 
-    fun esFuerte(): Boolean {
+    override fun esFuerte(): Boolean {
         TODO("Así me implementas mejor")
     }
 }
 
-class Planta(var altura: Double, val anioSemilla: Int) {
-    fun espacio() {
-        TODO("Pasaron cosas")
-    }
-
+abstract class Planta(var altura: Double, val anioSemilla: Int) {
+    abstract fun espacio(): Double
+    open fun esFuerte() = this.horasDeSolQueTolera() > 9
+    abstract fun horasDeSolQueTolera(): Int
+    open fun daSemillas() = this.esFuerte()
 }
