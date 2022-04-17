56d12d8a6b2832ec4bb97fba149027c843211f02
Carolina Luján <carolinaanahi.lujan@estudiantes.unahur.edu.ar>
2022-04-17 14:38:39 -0300

    Se agregó clase parcelas industriales pero no hace nada aún
    Y la planta, la pensé, la escribí pero no estaba en ningún lado(?)

--- src/main/kotlin/ar/edu/unahur/obj2/semillas/Parcelas.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/semillas/Parcelas.kt
@@ -22,11 +22,11 @@ package ar.edu.unahur.obj2.semillas
      //abstract fun seAsociaBien(planta: Planta): Boolean
 }
 
-class ParcelasEcologicas(ancho: Double, largo: Double, horasDeSol: Int): Parcelas(ancho, largo, horasDeSol) { //¿Por qué el ! en rojo?
+class ParcelasEcologicas(ancho: Double, largo: Double, horasDeSol: Int, planta: Planta): Parcelas(ancho, largo, horasDeSol) { //¿Por qué el ! en rojo?
     //override fun seAsociaBien(planta: Planta): Boolean { return !this.tieneComplicaciones() && planta.laParcelaEsIdeal(this) }
 }
 
-class ParcelasIndustriales(ancho : Double, largo: Double, horasDeSol: Int): Parcelas(ancho, largo, horasDeSol) {
+class ParcelasIndustriales(ancho : Double, largo: Double, horasDeSol: Int, planta: Planta): Parcelas(ancho, largo, horasDeSol) {
     //override fun seAsociaBien(planta: Planta): Boolean { return (this.cantidadMaximaDePlantas().size <= 2 && planta.esFuerte()) }
 }
 
